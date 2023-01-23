736fd7ba465a4475c3aff481ea5056d9fa311088
crystalhwaang <chwang610@gmail.com>
2023-01-23 15:17:09 -0700

    board creation

--- Project_1/Program.cs
+++ Project_1/Program.cs
@@ -6,7 +6,23 @@ namespace Project_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            static void GameBoard(Driver d)
+            {
+                d.arrayGameBoard;
+            }
+            static void PrintBoard()
+            {
+                Console.WriteLine("     |     |     ");
+                Console.WriteLine("  {0}|  {1}|  {2}", GameBoard[1], GameBoard[2], GameBoard[3]);
+                Console.WriteLine("_____|_____|_____");
+                Console.WriteLine("     |     |     ");
+                Console.WriteLine("  {0}|  {1}|  {2}", GameBoard[4], GameBoard[5], GameBoard[6]);
+                Console.WriteLine("_____|_____|_____");
+                Console.WriteLine("     |     |     ");
+                Console.WriteLine("  {0}|  {1}|  {2}", GameBoard[7], GameBoard[8], GameBoard[9]);
+                Console.WriteLine("     |     |     ");
+            }
+            static void CheckWinner()
         }
     }
 }
